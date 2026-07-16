35e26b8c7e7be98e118ca7065952d0a32fcd1e0a
Denis Goes <d.goesdev@gmail.com>
2026-07-16 19:34:54 -0300

    Update: main.py e webhook.py, preparando para deploy!

--- app/api/webhook.py
+++ app/api/webhook.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter
 import telebot
-import os
-from dotenv import load_dotenv
 
 from app.services.telegram import bot
 
-load_dotenv()
-
-API_TOKEN = os.getenv("API_TOKEN")
-
 router = APIRouter()
 
-@router.post(f"/webhook/{API_TOKEN}")
+
+@router.post("/webhook")
 async def webhook(update: dict):
+
     if update:
         update = telebot.types.Update.de_json(update)
         bot.process_new_updates([update])
--- app/main.py
+++ app/main.py
@@ -11,11 +11,19 @@ from app.services.telegram import bot
 load_dotenv()
 
 API_TOKEN = os.getenv("API_TOKEN")
+API_URL = os.getenv("API_URL")
 
 WEBHOOK_URL = (
-    f"https://dazzling-destitute-fragrance.ngrok-free.dev"
-    f"/webhook/{API_TOKEN}"
+    f"{API_URL}"
+    f"/webhook"
 )
+
+
+# WEBHOOK_URL = ( # Usada em desenvolvimento e testes locais
+#     f"https://dazzling-destitute-fragrance.ngrok-free.dev"
+#     f"/webhook/{API_TOKEN}"
+# )
+
 
 
 @asynccontextmanager
@@ -38,3 +46,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(router)
+
+@app.get("/healthz")
+def health_check():
+    return {"status": "ok"}
