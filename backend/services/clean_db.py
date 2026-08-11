1f0d08cb08565e91aea20c204ad17c67e2ee192f
Denis <d.goesdev@gmail.com>
2026-08-11 15:04:28 -0300

    Atualizado clean db

--- backend/services/clean_db.py
+++ backend/services/clean_db.py
@@ -1,24 +1,24 @@
 from backend.database.connection import SessionLocal
-from backend.database.model import Vaga
+from backend.database.model import Noticias
 from sqlalchemy import select, or_
 from datetime import datetime, UTC
 
 def clean_dados():
-    session = SessionLocal()
     agora = datetime.now(UTC)
-    try:
-        vagas = session.execute(
-            select(Vaga).where(Vaga.remover_em <= agora)
-        ).scalars().all()
+    with SessionLocal() as session:
+        try:
+            noticias = session.execute(
+                select(Noticias).where(Noticias.remover_em <= agora)
+            ).scalars().all()
 
-        if vagas:
-            for vaga in vagas:
-                session.delete(vaga)
+            if noticias:
+                for noticia in noticias:
+                    session.delete(noticia)
 
-            session.commit()
+                session.commit()
 
-        return len(vagas)
+            return len(noticias)
 
-    except Exception as e:
-        session.rollback()
-        print(f"Algo inesperado aconteceu! Rollback executado! {e}")
+        except Exception as e:
+            session.rollback()
+            raise
